4386f6274ab0fa8d0b159324cb3adaea6f878e4c
reconrez <reconrez@gmail.com>
2024-01-15 18:19:46 +0530

    removed the jade engine

--- app.js
+++ app.js
@@ -22,11 +22,6 @@ mongoose.connect(uri)
   .catch(err =>
     console.error('Error connecting to MongoDB:', err));
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
