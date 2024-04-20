b5ad9a1c32dc2eeb7d93646fcc4b6c16eeb084f6
reconrez <reconrez@gmail.com>
2024-04-20 19:05:28 +0530

    removed the post lazy loading

--- view/src/app/pages/feed/feed.component.ts
+++ view/src/app/pages/feed/feed.component.ts
@@ -20,26 +20,17 @@ export class FeedComponent implements OnInit {
 
   ngOnInit(): void {
     this.getPosts()
-    this.setupIntersectionObserver();
   }
   getPostData :any
   postData : any = []
   userData : any = []
-  displayedPosts: any = []; // Array to hold currently displayed posts
-  currentPage = 1;
-  pageSize = 2; // Number of posts to show per page
-  hasMorePosts = true;
-  isLoading = false; // Flag to indicate loading state
 
   getPosts(){
     this.getPostData = new Promise<any>((response, reject)=>{
     this.postService.getAllPosts().subscribe((res: any) => {
       this.postData = res;
-      this.displayedPosts = this.postData.slice(0, this.pageSize);
-      this.isLoading = false; // Set loading flag to false
       (error: any) => {
         catchError(error)
-        this.isLoading = false; // Set loading flag to false
       }
       if(this.postData){
         response(this.postData)
@@ -51,30 +42,5 @@ export class FeedComponent implements OnInit {
    this.getPostData.then(()=>{
     console.log(this.postData)
     })
-  }
-  trackByPostId(index, post): number {
-    return post.id; // Ensure proper tracking by unique post ID
-  }
-  setupIntersectionObserver() {
-    const observer = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && this.hasMorePosts && !this.isLoading) {
-        console.log("loaing more posts");
-        this.loadMorePosts();
-      }
-    });
-    observer.observe(document.body);
-  }
-
-  loadMorePosts() {
-    console.log(this.currentPage, this.pageSize );
-    this.isLoading = true; // Set loading flag to true
-    const startIndex = this.currentPage * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    this.displayedPosts = this.postData.slice(0, endIndex);
-    this.currentPage++;
-
-    // Check if there are more posts to load
-    this.hasMorePosts = endIndex < this.postData.length;
-    this.isLoading = false; // Set loading flag to false
   }
 }
